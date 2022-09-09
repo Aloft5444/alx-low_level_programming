3e85a004599bb1bd5e05e53d43c38810deb3e2cb
Aloft5444 <aloftthenegotiator@gmail.com>
2022-09-09 10:05:36 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -9,8 +9,14 @@ int main(void)
 {
 	char a;
 
-	for (a = 'z'; a >= 'a' ; a--)
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
 		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
